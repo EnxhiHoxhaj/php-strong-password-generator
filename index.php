83848ca56c9ab33a6745d8cf57e8b7e777a0b221
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-25 15:20:10 +0200

    feat: layout base without css

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Password generator!</title>
 </head>
 <body>
+    <div>
+        <h1>STRONG PASSWORD GENERATOR</h1>
+        <h2>Genera una password sicura</h2>
+        <div>Genera una password di lunghezza compresa fra 8 e 32</div>
+        <form action="">
+            <label for="">Lunghezza password: </label>
+            <input type="number">
+        </form>
+    </div>
     
 </body>
 </html>
