a4d0b9013c51fd0348464777ede9f5e4a01a1b87
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-25 18:42:09 +0200

    feat: end

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
  <?php 
 
-    function generaPassword($numeroCaratteri){
+    function generaPassword($n){
         $caratteri = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789,;.:-_ò@çà°#ù§[]{}()!£$%&/=?^*";
         $passwordGenerata='';
-        for($i = 0; $i< $numeroCaratteri; $i++){
-            $random= rand(0, strlen($caratteri));
+        for($i = 0; $i< $n; $i++){
+            $random= rand(0, strlen($caratteri)-1);
+            echo ($random);
             $passwordGenerata .= $caratteri[$random];
         }
         return $passwordGenerata;
